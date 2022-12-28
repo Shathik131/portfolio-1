6f97072a93a439eaee9d1b7f6884211f0adb12cd
Shathik131 <sathiksathik126@gmail.com>
2022-12-27 21:05:51 -0800

    Portfolio site

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*============== tying annimation ==============*/
+
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Web Developer",
+    "WordPress Developer",
+    "YouTuber",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
